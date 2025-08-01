a746e51427a36ce50b6722d94b096971d5d892f8
krsnapaudel <dilli@alpenlabs.io>
2025-08-01 16:02:41 -0600

    feat (frontend): frontend for balances

--- frontend/src/providers/ConfigProvider.tsx
+++ frontend/src/providers/ConfigProvider.tsx
@@ -7,6 +7,7 @@ export interface AppConfig {
   alpenExplorerUrl: string;
   bridgeStatusRefetchIntervalS: number;
   networkStatusRefetchIntervalS: number;
+  balanceRefetchIntervalS: number;
   environment: string;
 }
 
